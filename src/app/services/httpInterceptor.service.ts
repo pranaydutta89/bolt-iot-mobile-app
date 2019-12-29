4fdef795ca7f47246af3dc2f2b24d541d3421e5a
pranay dutta <pranaydutta89@gmail.com>
2019-12-29 15:44:45 +0530

    fixed loader

--- src/app/services/httpInterceptor.service.ts
+++ src/app/services/httpInterceptor.service.ts
@@ -7,7 +7,7 @@ import {
   HttpErrorResponse
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, from } from 'rxjs';
 import { StorageService } from './storage.service';
 import { IUserLoginData } from '../interfaces/interface';
 import { StorageData } from '../enums';
@@ -24,17 +24,16 @@ export class TokenInterceptorService implements HttpInterceptor {
     this.init();
   }
 
-  async init() {
-    const load = await this.loadingController.create({
-      message: 'Loading...'
-    });
-    this.loading = load;
-  }
+  async init() {}
 
   async toggleLoader(show = true) {
     if (this.loading) {
       if (show) {
         if (this.loaderCount === 0) {
+          const load = await this.loadingController.create({
+            message: 'Loading...'
+          });
+          this.loading = load;
           this.loaderCount += 1;
           await this.loading.present();
         }
@@ -51,34 +50,30 @@ export class TokenInterceptorService implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    // We retrieve the token, if any
-    this.toggleLoader();
+    return from(this.handle(req, next));
+  }
+
+  async handle(req: HttpRequest<any>, next: HttpHandler) {
+    const loader = await this.loadingController.create({
+      message: 'Loading...'
+    });
+
+    await loader.present();
+
     const { jwtToken } = this.storage.getData<IUserLoginData>(
       StorageData.userData
     );
     let newHeaders = req.headers;
     if (jwtToken) {
-      // If we have a token, we append it to our new headers
       newHeaders = newHeaders.append('authorization', jwtToken);
     }
-    // Finally we have to clone our request with our new headers
-    // This is required because HttpRequests are immutable
     const authReq = req.clone({ headers: newHeaders });
-    // Then we return an Observable that will run the request
-    // or pass it to the next interceptor if any
-    return next.handle(authReq).pipe(
-      // We use the map operator to change the data from the response
 
-      map(evt => {
-        if (evt instanceof HttpResponse) {
-          this.toggleLoader(false);
-          return evt;
-        }
-      }),
-      catchError((err: any) => {
-        this.toggleLoader(false);
-        return of(err);
-      })
-    );
+    return next
+      .handle(authReq)
+      .toPromise()
+      .finally(async () => {
+        await loader.dismiss();
+      });
   }
 }
